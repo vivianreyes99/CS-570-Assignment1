4fdc292fd33a622acb916673120174b774c71344
anoodnyas <58711029+anoodnyas@users.noreply.github.com>
2021-01-30 22:09:54 -0800

    AS - deleted old commented code

--- myls.cpp
+++ myls.cpp
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
+//#include <unistd.h>
 using namespace std;
 
 //Vivians local machine version
@@ -25,7 +25,7 @@ int main (int argc, char *argv[])
     // still need to add the output for just "myls", -h check
     // testing prints dots before the filename ?? idk why we have to figure that out
 
-    bool hidden = true;	
+    bool show_all = false;	
 
 	if (argc == 1) //the user only entered "myls" 
     {
@@ -39,7 +39,7 @@ int main (int argc, char *argv[])
 		if (strcmp(argv[i], "-h") == 0) // user entered "-h"
     	{
         	//set flag
-        	hidden = false;
+        	show_all = true;
     	}
 		
 		DIR *directory_path = NULL;
@@ -51,58 +51,13 @@ int main (int argc, char *argv[])
 		}
 		else 
 		{
-			while ( (d_pathtr = readdir(directory_path)) != NULL )
-			{
-				cout << d_pathtr->d_name << "\n";
-			}
+            //if (hidden == true) //&&  // if it starts with ., don't print it
+            {
+                while ( (d_pathtr = readdir(directory_path)) != NULL )
+			    {
+				    cout << d_pathtr->d_name << "\n";
+			    }
+            }
 		}
 	}
-
-    /*if (argc == 1) //the user only entered "myls" 
-    {
-        //print all the directories in the current path
-    }
-    
-    if (strcmp(argv[1], "-h") == 0) // user entered "-h"
-    {
-        //set flag
-        cout << argv[1] << "\n"; 
-    }
-
-    cout << argv[0] << "\n"; // print myls
-
-
-
-
-    /*
-    //DIR *directorypath = NULL;
-    //directorypath = opendir(directoryname)
-
-    //myls dir1
-    string rawInput;
-    vector<String> in;
-    while (getline(cin, rawInput, " ")){
-        in.push_back(rawInput); // in is a vector with the inputs
-    }
-    
-    
-    
-    for (auto i = in.begin(); i != in.end(); ++i){
-        if (i )// if == myls 
-            //continue;
-        if // if == -h
-            // set flag
-    } 
-
-    //std::vector<int> a = {2,4,3,5,6};
-   print(in); 
-   return 0; 
-   */
-}
-
-void print(std::vector <int> const &a) {
-   std::cout << "The vector elements are : ";
-
-   for(int i=0; i < a.size(); i++)
-   std::cout << a.at(i) << ' ';
 }

33d989709a93f9950003dda636abe462159010fa
Vivian Reyes <vivreyes99@gmail.com>
2021-02-01 21:58:33 -0800

    program working!! Need to refactor
    need to refactor code to avoid repetitive code

--- myls.cpp
+++ myls.cpp
@@ -7,18 +7,26 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-// #include <unistd.h>
 using namespace std;
 using std::cout;
 using std::endl;
 
 int main (int argc, char *argv[])
 {
+    //boolean for the -h flag, if show_all is true 
+    //then show all files including the hidden files
     bool show_all = false;
 
-    //the user only entered "myls" 
-	if (argc == 1)
-    { 
+    //the user only entered "myls" or "myls -h"
+	if (argc == 1 || (strcmp(argv[1], "-h") == 0 && argc == 2))
+    {
+        // user entered "myls -h"
+        if (argc == 2) 
+    	{
+        	//set flag to true since user typed in -h and wants to show hidden files
+        	show_all = true;
+    	}
+        
         //pointer to directory stream of type DIR, DIR is a structure that represents a directory stream
         DIR *directory_path = NULL;
         //name of files/what's in directory is stored in d_pathr, 
@@ -33,19 +41,23 @@ int main (int argc, char *argv[])
 		}
 		else 
 		{
-                //if readdir fails, d_pathr will be null and while loop ends, 
-                //else, d_pathr will be a dirent struct and readdir returns the next d_pathr 
-                // which are files inside the current directory
-                while ( (d_pathtr = readdir(directory_path)) != NULL )
-			    {
-                    //prints out all the files in the directory_path stream using the d_name field
-				    cout << d_pathtr->d_name << "\n";	
-                }		      
+            //if readdir fails, d_pathr will be null and while loop ends, 
+            //else, d_pathr will be a dirent struct and readdir returns the next d_pathr 
+            // which are files inside the current directory
+            while ( (d_pathtr = readdir(directory_path)) != NULL )
+			{
+            	// if no flag and it starts with a dot, continue to next iteration 
+				// (if it starts with ., don't print it)
+				if (show_all == false && d_pathtr->d_name[0] == '.')
+				{   
+					continue;
+				}
+                // cout << d_pathtr->d_name << "\n";
+				cout << d_pathtr->d_name << "\n"; 
+            }		      
 		}
     }
     
-    
-
 	for (int i = 1; i < argc; i++) // loop through each command line argument
 	{
 		if (strcmp(argv[i], "-h") == 0) // user entered "-h"
@@ -53,17 +65,21 @@ int main (int argc, char *argv[])
         	//set flag
         	show_all = true;
 			continue;
+
     	}
 		
 		DIR *directory_path = NULL; // has the path of the directory
 		struct dirent *d_pathtr = NULL; // struct contains the directory entries
 		
+        //opendir(".")
 		if ((directory_path = opendir(argv[i])) == NULL) //check if the directory exists
 		{
 			cout << "Cannot access " << argv[i] << "\n";
 		}
 		else 
 		{
+            cout << argv[i] << "\n";
+
             while ( (d_pathtr = readdir(directory_path)) != NULL ) //adds current directory entry to the struct
 			{
 				// if no flag and it starts with a dot, continue to next iteration 
